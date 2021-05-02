357ddea7c05ae67f95c75d1480eecddbb20bd599
nclsbayona <nbayonaord@gmail.com>
2021-05-02 18:20:10 -0500

    index

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
+//Credits to @thmsgrbt --> https://github.com/thmsgbrt
 require('dotenv').config();
 const Mustache = require('mustache');
 const fetch = require('node-fetch');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './mustache-main';
-var city="Bogota"
+const puppeteerService = require('./puppeteer-service');
+var query = "Bogota,CO,"
+var igAccount = 'nclsbayona'
 let DATA = {
     refresh_date: new Date().toLocaleDateString('en-GB', {
-      weekday: 'long',
-      month: 'long',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      timeZoneName: 'short',
-      timeZone: 'America/Bogota',
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZoneName: 'short',
+        timeZone: 'America/Bogota',
     }),
-  };
+};
+
+async function setWeatherInformation() {
+    await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${{ query }}&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+    )
+        .then(r => r.json())
+        .then(r => {
+            DATA.city_temperature = Math.round(r.main.temp);
+            DATA.city_weather = r.weather[0].description;
+            DATA.city_weather_icon = r.weather[0].icon;
+            DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
+                hour: '2-digit',
+                minute: '2-digit',
+                timeZone: DATA.timeZone,
+            });
+            DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
+                hour: '2-digit',
+                minute: '2-digit',
+                timeZone: DATA.timeZone,
+            });
+        });
+};
+
+async function setInstagramPosts() {
+    const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount(igAccount, 3);
+    DATA.img1 = instagramImages[0];
+    DATA.img2 = instagramImages[1];
+    DATA.img3 = instagramImages[2];
+}
+
+async function generateReadMe() {
+    await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+        if (err) throw err;
+        const output = Mustache.render(data.toString(), DATA);
+        fs.writeFileSync('README.md', output);
+    });
+}
+
+async function action() {
+    //Fetch Weather
+    await setWeatherInformation();
+
+    //Get pictures
+    await setInstagramPosts();
+
+    //Generate README
+    await generateReadMe();
+
+    //Close resources
+    await puppeteerService.close();
+}
+
+action();
