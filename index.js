b88eb4228914f926de05ccb5658f2af668b562e2
nclsbayona <nbayonaord@gmail.com>
2021-05-02 20:04:49 -0500

    .

--- index.js
+++ index.js
@@ -20,25 +20,23 @@ let DATA = {
 };
 
 async function setWeatherInformation() {
-    await fetch(
+    let r = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${{ query }}&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
-    )
-        .then(r => r.json())
-        .then(r => {
-            DATA.city_temperature = Math.round(r.main.temp);
-            DATA.city_weather = r.weather[0].description;
-            DATA.city_weather_icon = r.weather[0].icon;
-            DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
-                hour: '2-digit',
-                minute: '2-digit',
-                timeZone: DATA.timeZone,
-            });
-            DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
-                hour: '2-digit',
-                minute: '2-digit',
-                timeZone: DATA.timeZone,
-            });
-        });
+    );
+    DATA.r = r;
+    DATA.city_temperature = Math.round(r.main.temp);
+    DATA.city_weather = r.weather[0].description;
+    DATA.city_weather_icon = r.weather[0].icon;
+    DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: DATA.timeZone,
+    });
+    DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: DATA.timeZone,
+    });
 };
 
 async function setInstagramPosts() {
@@ -46,9 +44,6 @@ async function setInstagramPosts() {
     DATA.img1 = instagramImages[0];
     DATA.img2 = instagramImages[1];
     DATA.img3 = instagramImages[2];
-    console.log(DATA.img1)
-    console.log(DATA.img2)
-    console.log(DATA.img3)
 }
 
 async function generateReadMe() {
